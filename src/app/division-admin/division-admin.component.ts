ad39c18dd5a5586b3ec833f935ccdcfd74f28f4b
ardian <ardianmacascas0531@gmail.com>
2025-04-05 00:08:15 +0800

    navigation

---
+++ src/app/division-admin/division-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-division-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './division-admin.component.html',
+  styleUrl: './division-admin.component.css'
+})
+export class DivisionAdminComponent {
+
+}
---
+++ src/app/school-admin/school-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-school-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './school-admin.component.html',
+  styleUrl: './school-admin.component.css'
+})
+export class SchoolAdminComponent {
+
+}
