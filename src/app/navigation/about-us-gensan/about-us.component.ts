5b54a1e4beb5c668057632aeca199fabc105fa19
ardian macascas <163614470+ardianmacascas0531@users.noreply.github.com>
2025-09-30 02:20:33 +0800

    nav. disable and about page
    disabled some nav menu , adding UI for about page

---
+++ src/app/navigation/about-us-gensan/about-us.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from "@angular/material/card";
+
+@Component({
+  selector: 'app-about-us',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './about-us.component.html',
+  styleUrl: './about-us.component.css'
+})
+export class AboutUsComponent {
+
+}
