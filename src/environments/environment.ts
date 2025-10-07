a6335c467e6ff872411d9b5c2d6e99cf4a6e15a0
ardian macascas <163614470+ardianmacascas0531@users.noreply.github.com>
2025-10-07 11:02:40 +0800

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ import {commonEnvironment} from "./environment.common";
 export const environment = {
   ...commonEnvironment,
   production: false,
-  API_URL: 'http://localhost:300'
+  API_URL: 'http://localhost:3000'
 };
